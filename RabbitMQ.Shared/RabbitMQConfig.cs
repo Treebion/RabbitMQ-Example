05d71f6427ad2e6925f98fe771fb7853a32a8f4f
FlopsyTheBunny <johnlhird@btinternet.com>
2025-02-11 12:46:39 +0000

    Rename folder to match project name Remove unnecessary usings

--- RabbitMQ.Shared/RabbitMQConfig.cs
+++ RabbitMQ.Shared/RabbitMQConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RabbitMQ.Shared
+﻿namespace RabbitMQ.Shared
 {
     /// <summary>
     /// Represents the configuration settings for RabbitMQ.
